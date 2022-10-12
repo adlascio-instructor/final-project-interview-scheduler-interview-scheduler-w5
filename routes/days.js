e562a8dd8b0cc17f0177b71f46a3d551f7577562
Megumi Akama <kamayatu91@gmail.com>
2022-10-12 16:46:46 -0700

    Create credentials

--- routes/days.js
+++ routes/days.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const { Pool } = require("pg");
+const credentials = require('../shared/credentials');
 
 const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.send('Days route is on!');
+// Days route (Get all days and quantity of appointments available)
+router.get('/', async (req, res) => {
+    const pool = new Pool(credentials);
+    const response = await pool.query('SELECT * FROM days');
+    res.json(response.rows);
+    pool.end();
 });
 
 module.exports = router;
