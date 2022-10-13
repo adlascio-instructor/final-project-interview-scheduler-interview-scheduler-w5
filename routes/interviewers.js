2e0b6c1f0fc216368b9a08259e8a452cc08ebbc2
Mauricio Tomaz <mauriciotomazz@gmail.com>
2022-10-13 16:15:39 -0700

    Implement interviews and interviewers routes.

--- routes/interviewers.js
+++ routes/interviewers.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const { Pool } = require("pg");
+const credentials = require('../shared/credentials');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Interviewers route is on!');
+router.get('/available/:dayId', async (req, res) => {
+    const { dayId } = req.params;
+
+    const pool = new Pool(credentials);
+    const response = await pool.query(
+        `SELECT interviewers.*
+        FROM interviewers INNER JOIN available_interviewers
+        ON interviewers.id = available_interviewers.interviewer_id
+        WHERE available_interviewers.day_id = $1`,
+        [dayId]
+    );
+
+    res.json(response.rows);
+    pool.end();
 });
 
 module.exports = router;
--- routes/interviews.js
+++ routes/interviews.js
@@ -1,9 +1,42 @@
 const express = require('express');
+const { Pool } = require("pg");
+const credentials = require('../shared/credentials');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Interviews route is on!');
+router.get('/:dayId', async (req, res) => {
+    const { dayId } = req.params;
+
+    const pool = new Pool(credentials);
+    const response = await pool.query(
+        `SELECT appointments.id, appointments.time, interviews.id as interview_id, interviews.student,
+        interviewers.id as interviewer_id, interviewers.name as interviewer_name,
+        interviewers.avatar as interviewer_avatar
+        FROM appointments LEFT JOIN interviews ON appointments.id = interviews.appointment_id
+        LEFT JOIN interviewers ON interviews.interviewer_id = interviewers.id
+        WHERE day_id = $1`,
+        [dayId]
+    );
+
+    const data = response
+        .rows
+        .map(r => ({
+            id: r.id,
+            time: r.time,
+            interview: r.interview_id ? {
+                id: r.interview_id,
+                student: r.student,
+                interviewer: {
+                    id: r.interviewer_id,
+                    name: r.interviewer_name,
+                    avatar: r.interviewer_avatar,
+                }
+            } : undefined,
+        }))
+        .reduce((dataObj, r) => ({ ...dataObj, [r.id]: r }), {});
+
+    res.json(data);
+    pool.end();
 });
 
 module.exports = router;
